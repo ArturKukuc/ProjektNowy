94e84a58af733f3187eca86a34cca179400836d6
Artur Kukuć <akukuc8@gmail.com>
2020-10-26 19:50:21 +0100

    Add showProblem() function

--- src/Tablet.java
+++ src/Tablet.java
@@ -16,5 +16,8 @@ public class Tablet {
         int number = 79;
         return 2+60+number;
     }
+
+    public void showProblem(){
+        System.out.println("Tablet się zrypał");
+    }
 }
-//Dodaj nowy branch git branch -b testowyTabletCyfrowy działaj na nim daj commit wróć do main gałęzi popracuj i pousuwaj pliki znajdujące sie w folderze a potem spróbuj zmergować projekty, popróbuj różnych konfiguracji
